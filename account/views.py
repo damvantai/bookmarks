91463ff8c1ca462e4a96d645ca768a20f3429715
damvantai <tai94bn@gmail.com>
2017-08-15 21:15:35 +0700

    register and add gui to login

--- account/views.py
+++ account/views.py
@@ -37,3 +37,12 @@ def register(request):
             new_user = user_form.save(commit=False)
             # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, 'account/register.html',
+                  {'user_form': user_form})
